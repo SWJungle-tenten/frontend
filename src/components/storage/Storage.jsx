c0b966c3df016ecd227b503e355649dd9368243a
terry <kdjtiger@gmail.com>
2023-06-23 03:32:15 +0900

    fix: API & storage size

--- src/components/storage/Storage.jsx
+++ src/components/storage/Storage.jsx
@@ -3,18 +3,21 @@ import Scrap from "./Scrap";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 import Header from "../intro/Header";
+import Memo from "../../memo/Memo";
+import { useNavigate } from "react-router-dom";
 
 export default function Storage() {
   const [userScrapData, setData] = useState(null);
   const [cookies] = useCookies(["accessToken"]);
   const [isLoading, setIsLoading] = useState(true);
   const [userName, setUserName] = useState(null);
+  const go = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.post(
-          "http://localhost:8080/api/keyWordByDate",
+          "http://localhost:8080/api/checkStorage",
           {},
           {
             headers: {
@@ -40,12 +43,22 @@ export default function Storage() {
     <>
       <Header/>
       <div className="flex">
-        <div className="flex-grow">
-          {console.log(isLoading)}
-          {console.log(userScrapData)}
+        <div className="flex-grow w-[70%]">
           {!isLoading && userScrapData && (
             <Scrap userScrapData={userScrapData} userName={userName} />
           )}
+        </div>
+        <div className="w-[30%] border overflow-auto">
+          <div className="flex border-gray-300 shadow-lg overflow-auto">
+            <Memo />
+          </div>
+          <button
+            onClick={() => {
+              go("/main");
+            }}
+          >
+            dasdsadas
+          </button>
         </div>
       </div>
     </>
